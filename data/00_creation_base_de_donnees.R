9b9337db74513e355468dadb3fe1efee7010aeaf
Aurélie Fréchet <aurelie.frechet@gmail.com>
2024-04-18 17:15:11 +0200

    modifs scripts creations bdd

--- data/00_creation_base_de_donnees.R
+++ data/00_creation_base_de_donnees.R
@@ -7,19 +7,19 @@ options(digits = 14)
 # Chargement et nettoyage -------------------------------------------------
 
 ## Arbres ornements ----
-arbres_ornement    <- read.csv2("data/arbres-dornement-rennes.csv", encoding = "UTF-8")
+arbres_ornement    <- read.csv2("data/opendata_rennes/arbres-dornement-rennes.csv", encoding = "UTF-8")
 str(arbres_ornement)
 colnames(arbres_ornement)[1:2] <- c("geo_point", "geo_shape")
 arbres_ornement$donnees <- "Arbres d'ornement"
 
 ## Arbres alignements ----
-arbres_alignement  <- read.csv2("data/arbres-d-alignement-rennes.csv", encoding = "UTF-8")
+arbres_alignement  <- read.csv2("data/opendata_rennes/arbres-d-alignement-rennes.csv", encoding = "UTF-8")
 str(arbres_alignement)
 colnames(arbres_alignement)[1:2] <- c("geo_point", "geo_shape")
 arbres_alignement$donnees <- "Arbres d'alignement"
 
 ## Arbres Rennes metropole ----
-arbres_hors_rennes <- read.csv2("data/arbre_hors_rennes.csv", encoding = "UTF-8")
+arbres_hors_rennes <- read.csv2("data/opendata_rennes/arbre_hors_rennes.csv", encoding = "UTF-8")
 str(arbres_hors_rennes)
 colnames(arbres_hors_rennes)[1:2] <- c("geo_point", "geo_shape")
 arbres_hors_rennes$donnees <- "Arbres hors Rennes"
@@ -37,13 +37,18 @@ divide_geopoint <- data_arbres$geo_point %>% strsplit(',')
 data_arbres$lat = sapply(divide_geopoint, '[', 1) %>% as.numeric()
 data_arbres$lng = sapply(divide_geopoint, '[', 2) %>% as.numeric()
 
+non_identifie <- c("TEST", "NON", "", "SAISIE", "AUTRE", "RENSEIGNÉ", "NON RENSEIGNÉ", "SP", "SP.")
+
 data_arbres <- data_arbres %>%
   mutate(
+    genre         = ifelse(genre %in% non_identifie,   NA, genre),
+    espece        = ifelse(espece %in% non_identifie,  NA, espece),
+    variete       = ifelse(variete %in% non_identifie, NA, variete),
     nom_taxon     = stringr::str_trim(paste2(genre, espece, variete)),
     nom_latin     = stringr::str_trim(paste2(genre, espece)),
-    date_plant    = ifelse(date_plant < 1000, NA, date_plant),
+    date_plant    = ifelse(date_plant < 1000,  NA, date_plant),
     circonference = ifelse(circonference == 0, NA, circonference),
-    hauteur       = ifelse(hauteur == 0, NA, hauteur),
+    hauteur       = ifelse(hauteur == 0, NA, hauteur)
 
   ) %>%
   select(
@@ -64,38 +69,10 @@ data_arbres <- data_arbres %>%
 
 str(data_arbres)
 summary(data_arbres)
-sort(colnames(data_arbres))
 
 
 
 
-
-
-# Identification des arbres -----------------------------------------------
-
-
-# Uniquement les arbres identifiés
-## Genre
-sort(table(data_arbres$genre), decreasing = T)
-sort(unique(data_arbres$genre))
-data_arbres$genre_identifie <- ifelse(data_arbres$genre %in% c("TEST", "NON", "", "SAISIE", "AUTRE"), F, T)
-
-## Espece
-sort(table(data_arbres$espece), decreasing = T)
-sort(unique(data_arbres$espece))
-data_arbres$espece_identifie <- ifelse(data_arbres$espece %in% c("RENSEIGNÉ", "", "SAISIE", "AUTRE"), F, T)
-
-data_arbres$identifie <- data_arbres$genre_identifie & data_arbres$espece_identifie
-table(data_arbres$identifie, data_arbres$donnees)
-
-## Nom commun
-sort(table(data_arbres$nom_commun), decreasing = T)
-sort(unique(data_arbres$nom_commun))
-data_arbres$nom_identifie <- ifelse(data_arbres$espece %in% c("NON RENSEIGNÉ", "", "X"), F, T)
-table(data_arbres$nom_identifie, data_arbres$donnees)
-
-## Date plantation
-table(!is.na(data_arbres$date_plant), data_arbres$donnees)
 
 
 # Export ------------------------------------------------------------------
@@ -107,8 +84,10 @@ saveRDS(data_arbres, "data/arbres_rennes_metropole.RDS")
 
 
 # Genre & espece identifié OU nom_commun
+table(is.na(data_arbres$genre))
+
 arbres_identifies <- data_arbres %>%
-  dplyr::filter(identifie | nom_identifie)
+  dplyr::filter(!is.na(genre) & !is.na(espece))
 
 str(arbres_identifies)
 # 127 386 obs
@@ -123,7 +102,7 @@ saveRDS(arbres_identifies, "data/arbres_rennes_identifies.RDS")
 
 noms_a_corriger <-
   arbres_identifies %>%
-  group_by(nom_latin, genre, espece, variete, nom_commun) %>%
+  group_by(nom_latin, genre, espece, variete, nom_commun, nom_simplifie = nom_commun) %>%
   summarise(n = n())
 
 write.csv(noms_a_corriger, "data/Noms_arbres_a_corriger.csv")
--- data/enrichissement_donnees.R
+++ data/enrichissement_donnees.R
@@ -1,27 +1,28 @@
 library(readODS)
 library(dplyr)
 
-data_arbres <- readRDS("data/arbres_rennes_metropole.RDS")
+data_arbres <- readRDS("data/arbres_rennes_identifies.RDS")
 str(data_arbres)
 
 # Regroupement par nom taxon ----------------------------------------------
 
 varietes_arbres <- data_arbres %>%
-  dplyr::filter(identifie == "Identifié") %>%
-  group_by(nom_taxon, nom_commun) %>%
+  group_by(nom_latin, nom_commun) %>%
   summarise(n = n())
 
 write.csv(varietes_arbres, "data/variete_arbres.csv")
 
 
-
 # Import correction noms  -------------------------------------------------
 # correspondance nom taxon et nom vernaculaire
-varietes_new <- read_ods("../VARIETES.ods",row_names = TRUE,as_tibble = FALSE)
+varietes_new <-
+  read_ods("data/VARIETES.ods",
+           row_names = TRUE,
+           as_tibble = FALSE)
 str(varietes_new)
 
 data_arbre2 <- data_arbres  %>%
-  left_join(varietes_new, by = c("nom_commun","nom_taxon")) %>%
+  left_join(varietes_new, by = c("nom_commun", "nom_taxon")) %>%
   mutate(nom_vernaculaire = new_nom_commun)
 
 sort(unique(varietes_new$nom_taxon))
@@ -31,5 +32,23 @@ View(data_arbre2)
 length(unique(data_arbre2$nom_commun))
 length(unique(data_arbre2$nom_vernaculaire))
 
+saveRDS(data_arbre2, "data/arbres_identifies_simplifies.RDS")
 
-saveRDS(data_arbre2,"data/arbres_identifies_simplifies.RDS" )
+
+# Donnees arboretum -------------------------------------------------------
+# Lecture données
+arboretum <-
+  readxl::read_excel("data/arboretum/donnees_arboretum.xlsx")
+str(arboretum)
+# Recheche correspondance
+arboretum$nom_latin <- toupper(arboretum$nom_latin)
+
+
+non_repertories <-
+  setdiff(arboretum$nom_latin, unique(data_arbre2$nom_latin))
+arboretum[arboretum$nom_latin %in% non_repertories, ]
+
+jointure_arboretum <-
+  data_arbre2 %>% left_join(arboretum, by = "nom_latin")
+
+saveRDS(jointure_arboretum, "data/jointure_arboretum.RDS")
